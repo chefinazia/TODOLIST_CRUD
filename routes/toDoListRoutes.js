e78df379301ccb11b8b8e99e3e56d2bb926033f6
Rachit <rachit.arora@pristyncare.com>
2022-08-28 20:14:31 +0530

    add routes of all controllers

--- routes/toDoListRoutes.js
+++ routes/toDoListRoutes.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let router = express.Router();
 let toDoListConttrollers = require('../controllers/toDoListController')
-router.post("createTask",toDoListConttrollers.createTask)
+
+router.post("/createTask",toDoListConttrollers.createTask)
+router.post("/getAllTask",toDoListConttrollers.getAllTask)
+router.post("/getATask",toDoListConttrollers.getATask)
+router.post("/updateTask",toDoListConttrollers.updateTask)
+router.post("/deleteTask",toDoListConttrollers.deleteTask)
 
 module.exports = router;
